d7a2e5ff901ad4a1ff929172a428d60699ba93f9
georgetmeier <georgetmeier@hotmail.com>
2021-01-29 22:51:30 -0800

    refactor error check and add doctest

--- csv_grab.py
+++ csv_grab.py
@@ -2,6 +2,66 @@ import pandas as pd
 import json
 import sys
 
+
+def errorCheck(pf, sf, myDict):
+    '''
+    >>> myDict={'secondaryKeyPos': 1, 'primaryKeyPos': 0, 'grabPos': (2,)}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR with dictionary key names
+
+    >>> myDict={'primaryKeyPos': 0, 'secondaryKeyPos': 1, 'grabPos': 1}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR grabPos must be tuple
+
+    >>> myDict={'primaryKeyPos': 0, 'secondaryKeyPos': 1, 'grabPos': (3,)}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR dictionary element out of range
+
+    >>> myDict={'primaryKeyPos': 0, 'secondaryKeyPos': 1, 'grabPos': (-1,)}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR dictionary element out of range
+
+    >>> myDict={'primaryKeyPos': -1, 'secondaryKeyPos': 1, 'grabPos': (2,)}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR dictionary element out of range
+
+    >>> myDict={'primaryKeyPos': 3, 'secondaryKeyPos': 1, 'grabPos': (2,)}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR dictionary element out of range
+
+    >>> myDict={'primaryKeyPos': 0, 'secondaryKeyPos': -1, 'grabPos': (2,)}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR dictionary element out of range
+
+    >>> myDict={'primaryKeyPos': 0, 'secondaryKeyPos': 3, 'grabPos': (2,)}; a = pd.DataFrame(data={'x': ['a', 'b', 'c'], 'y': ['a','b','c'], 'z': [1,2,3]});errorCheck(a,a,myDict)
+    Traceback (most recent call last):
+    ...
+    Exception: ERROR dictionary element out of range
+    '''
+    # error check myDict
+    if [key for key in myDict.keys()] != ['primaryKeyPos', 'secondaryKeyPos', 'grabPos']:
+        raise Exception('ERROR with dictionary key names')
+
+    # error check tuple
+    if type(myDict['grabPos']) is not tuple:
+        raise Exception('ERROR grabPos must be tuple')
+
+    # error check 0<=keys<=cols
+    if myDict['primaryKeyPos'] > len(pf.columns) - 1 \
+            or myDict['secondaryKeyPos'] > len(sf.columns) - 1 \
+            or True in [myDict['grabPos'][i] > len(sf.columns) - 1 for i in range(len(myDict['grabPos']))] \
+            or myDict['primaryKeyPos'] < 0 \
+            or myDict['secondaryKeyPos'] < 0 \
+            or True in [myDict['grabPos'][i] < 0 for i in range(len(myDict['grabPos']))]:
+        raise Exception('ERROR dictionary element out of range')
+
 def csv_grab(primaryFile, secondaryFile, outputFile, myDict):
     '''
     myDict = {'primaryKeyPos': #, 'secondaryKeyPos': #, 'grabPos': (#,)}
@@ -14,22 +74,7 @@ def csv_grab(primaryFile, secondaryFile, outputFile, myDict):
     pf = pd.read_csv(primaryFile)
     sf = pd.read_csv(secondaryFile)
 
-    # error check myDict
-    if [key for key in myDict.keys()] != ['primaryKeyPos', 'secondaryKeyPos', 'grabPos']:
-        raise Exception('ERROR with dictionary key names')
-
-    # error check tuple
-    if type(myDict['grabPos']) is not tuple:
-        raise Exception('ERROR grabPos must be tuple')
-
-    # error check 0<=keys<=cols
-    if myDict['primaryKeyPos'] > len(pf.columns)-1 \
-            or myDict['secondaryKeyPos'] > len(sf.columns) - 1 \
-            or True in [myDict['grabPos'][i] > len(sf.columns) - 1 for i in range(len(myDict['grabPos']))] \
-            or myDict['primaryKeyPos'] < 0 \
-            or myDict['secondaryKeyPos'] < 0 \
-            or True in [myDict['grabPos'][i] < 0 for i in range(len(myDict['grabPos']))]:
-        raise Exception('ERROR dictionary element out of range')
+    errorCheck(pf,sf,myDict)
 
     # check if primary match secondary
     pKeys = [i for i in pf[pf.columns[myDict['primaryKeyPos']]]]
@@ -53,7 +98,7 @@ def csv_grab(primaryFile, secondaryFile, outputFile, myDict):
 
 
 if __name__=='__main__':
-    if len(sys.argv) <= 1:
+    if len(sys.argv) <= 2:
         test = {'primaryKeyPos': 0, 'secondaryKeyPos': 1, 'grabPos': (2,)}
         csv_grab('test0.csv', 'test1.csv', 'out.csv', test)
     else:
