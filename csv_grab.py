65fc4047f0efc0e14a6627657f0a43591b08df82
georgetmeier <georgetmeier@hotmail.com>
2021-01-29 20:14:11 -0800

    added error check negative inputs

--- csv_grab.py
+++ csv_grab.py
@@ -5,19 +5,21 @@ def csv_grab(primaryFile, secondaryFile, outputFile, myDict):
     myDict = {'primaryKeyPos': #, 'secondaryKeyPos': #, 'grabPos': (#,)}
     grabPos is a tuple (a tuple of 1 looks like (#,))
     '''
-
-    # tuple check
-    if type(myDict['grabPos']) is not tuple:
-        raise Exception('grabPos must be tuple')
-
     # read in csv
     pf = pd.read_csv(primaryFile)
     sf = pd.read_csv(secondaryFile)
 
-    # error check keys in bounds
+    # error check tuple
+    if type(myDict['grabPos']) is not tuple:
+        raise Exception('grabPos must be tuple')
+
+    # error check 0<=keys<=cols
     if myDict['primaryKeyPos'] > len(pf.columns)-1 \
-            or myDict['secondaryKeyPos'] > len(sf.columns)-1 \
-            or True in [myDict['grabPos'][i] > len(sf.columns)-1 for i in range(len(myDict['grabPos']))]:
+            or myDict['secondaryKeyPos'] > len(sf.columns) - 1 \
+            or True in [myDict['grabPos'][i] > len(sf.columns) - 1 for i in range(len(myDict['grabPos']))] \
+            or myDict['primaryKeyPos'] < 0 \
+            or myDict['secondaryKeyPos'] < 0 \
+            or True in [myDict['grabPos'][i] < 0 for i in range(len(myDict['grabPos']))]:
         raise Exception('ERROR dictionary element out of range')
 
     # check if primary match secondary
@@ -43,5 +45,9 @@ def csv_grab(primaryFile, secondaryFile, outputFile, myDict):
 
 if __name__=='__main__':
     d = {'primaryKeyPos': 0, 'secondaryKeyPos': 1, 'grabPos': (2,)}
-    foo=csv_grab('test0.csv', 'test1.csv', 'out.csv', d)
+    csv_grab('test0.csv', 'test1.csv', 'out.csv', d)
 
+    try:
+        csv_grab('test0.csv', 'test1.csv', 'out.csv', {'primaryKeyPos': 0, 'secondaryKeyPos': 1, 'grabPos': (2,)})
+    except:
+        print('errors')
